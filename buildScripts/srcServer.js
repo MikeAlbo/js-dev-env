5d833e58dc2950b0f9444a0750abd40ba865e5cc
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-03-27 20:10:23 -0500

    http section

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,9 +19,18 @@ app.get('/', function (req, res) {
 		res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+	// mock db
+	res.json([
+		{"id" : 1, "firstName" : "Bob", "lastName" : "Smith", "email" : "bob@gmail.com"},
+		{"id" : 2, "firstName" : "Tammy", "lastName" : "Norton", "email" : "tnorton@gmail.com"},
+		{"id" : 3, "firstName" : "Tina", "lastName" : "lee", "email" : "lee.tina@gmail.com"},
+	]);
+});
+
 app.listen(port, function (err) {
 		if(err){
-			console.log(err);
+			console.log(err);  // eslint-disable-line no-console
 		} else {
 			open('http://localhost:' + port);
 		}
